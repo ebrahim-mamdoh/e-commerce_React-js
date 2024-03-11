fd1e85fb029fbaf137a2250b7dcbfcfd6b49653f
Ebrahim mamdouh <138860098+ebrahim-mamdoh@users.noreply.github.com>
2024-03-11 13:52:16 +0200

    usQuery to feach api

--- src/Component/FeaturedProducts/FeaturedProducts.jsx
+++ src/Component/FeaturedProducts/FeaturedProducts.jsx
@@ -1,38 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import style from "./FeaturedProducts.module.css"
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import style from "./FeaturedProducts.module.css";
+import axios from "axios";
+import { useQuery } from "react-query";
 
 export default function FeaturedProducts() {
+  function getFeatureProducts(){
+    return  axios.get(`https://ecommerce.routemisr.com/api/v1/products`);
+  }
+ let {data,isLoading,isFetching,isError}= useQuery('featuedProduct',getFeatureProducts)
+ //using null seafty??????? to verify from arrive the first data
+console.log(data?.data.data);
 
-  const [FeaturedProducts, setFeaturedProducts] = useState([])
-
+  // const [FeaturedProducts, setFeaturedProducts] = useState([])
   //return data from api and store it in var that name is data
-async function getFeatureProducts(){
- let {data}=await axios.get(`https://ecommerce.routemisr.com/api/v1/products`)
-setFeaturedProducts(data.data)
-}
+  // async function getFeatureProducts(){
+  //  let {data}=await axios.get(`https://ecommerce.routemisr.com/api/v1/products`)
+  // setFeaturedProducts(data.data)
+  // }
+  // useEffect(()=>{getFeatureProducts()},[])
 
-useEffect(()=>{getFeatureProducts()},[])
-
-
-  return <>
-  <div className="row">
-      {FeaturedProducts.map((product)=>(
-        <div key={product.id} className="col-md-2 gy-4 gx-4 ">
-          <img src={product.imageCover} className='w-100' alt="" />
-          <h2 className='font-sm text-main fw-bold'>{product.category.name}</h2>
-          <h2 className='h5 fw-bold '>{ product.title.split(' ').slice(0,2).join(" ")}</h2>
-          <div className="d-flex justify-content-between">
-            <span>{product.price}EGP</span>
-            <span>
-              <i className='fas fa-star rating-color'></i>
-              {product.ratingsAverage}
+  return (
+    <>
+      <div className="row">
+        {data?.data.data.map((product) => (
+          <div key={product.id} className="col-md-3 col-lg-2   gy-4 gx-4 ">
+            <img src={product.imageCover} className="w-100" alt="" />
+            <h2 className="font-sm text-main fw-bold">
+              {product.category.name}
+            </h2>
+            <h2 className="h5 fw-bold ">
+              {product.title.split(" ").slice(0, 2).join(" ")}
+            </h2>
+            <div className="d-flex justify-content-between">
+              <span>{product.price}EGP</span>
+              <span>
+                <i className="fas fa-star rating-color"></i>
+                {product.ratingsAverage}
               </span>
+            </div>
           </div>
-           </div>
-     ) )}
-  </div>
-  </>    
-
- 
+        ))}
+      </div>
+    </>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -5,14 +5,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import {ReactQueryDevtools} from 'react-query/devtools'
 
-
+let queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
-);
+    <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen='false' position='bottom-right'></ReactQueryDevtools>
+    </QueryClientProvider>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+);
